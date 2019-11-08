6883d3e06d9a718835907496489cba182ac9016d
Will Robinson <will@oznetnerd.com>
2019-11-08 11:39:19 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# OzNetNerd Deep Security
+
+A convenient way to interact with Trend Micro Deep Security.
+
+# Contact
+
+* Blog: oznetnerd.com
+* Email: will@oznetnerd.com
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='onnds',
+    version='0.1',
+    packages=['onnds'],
+    url='oznetnerd.com',
+    install_requires=['deep-security-api>=12.0.327'],
+    license='',
+    author='Will Robinson',
+    author_email='will@oznetnerd.com',
+    description='Convenient way to interact with Trend Micro Deep Security'
+)
